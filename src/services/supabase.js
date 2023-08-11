4ed191ca7da67ffc844356c0dba4ef32bf867816
Bryan <34315902+bryan-padero@users.noreply.github.com>
2023-08-11 20:46:02 +0400

    added create and edit cabin functionality

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://bbxtjmarmdffjahsmjkf.supabase.co";
+export const supabaseUrl = "https://bbxtjmarmdffjahsmjkf.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJieHRqbWFybWRmZmphaHNtamtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTE1NjQ3MjksImV4cCI6MjAwNzE0MDcyOX0.oIy0OCAzWIlPe8Ez_oqrGSFpPBk9lRdCd9PuqpeLDLE";
 const supabase = createClient(supabaseUrl, supabaseKey);
